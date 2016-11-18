223636920ea98775dcc32a539d2b18dadefa52ee
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2016-11-18 11:27:28 -0800

    finished feature engineering and wrote csv file, cleaned up cleaning script

--- src/modeling/outcomes_quiz/outcome_cleaning.py
+++ src/modeling/outcomes_quiz/outcome_cleaning.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 import copy
 import operator
@@ -14,15 +15,40 @@ def load_data(filename):
 def drop_features(df):
     """
     """
-    df_copy = copy.deepcopy(df[df['Major_category'] != 'Industrial Arts & Consumer Services'])
+    df_copy = copy.deepcopy(df[df['Major_category']
+                            != 'Industrial Arts & Consumer Services'])
     return df_copy
+
+def normalize_column(df, col):
+    """
+    Normalize a column by forcing it between 0 and 1
+    """
+    min_val = df[col].min()
+    max_val = df[col].max()
+    return (df[col] - min_val) / (max_val - min_val)
 
 def add_features(df):
     """
     """
-    df['full_time_employment_rate'] = df['Employed_full_time_year_round'] / df['Employed']
+    df['full_time_employment_rate'] = (df['Employed_full_time_year_round']
+                                       / df['Employed']
+                                       )
     df['pay_IQR_range'] = df['P75th'] - df['P25th']
     df['field_of_study'] = df.apply(field_majorcat_mapper, axis=1)
+    df['norm_ur'] = normalize_column(df, 'Unemployment_rate')
+    df['norm_ftr'] = normalize_column(df, 'full_time_employment_rate')
+    df['norm_med_income'] = normalize_column(df, 'Median')
+    df['norm_p25th_income'] = normalize_column(df, 'P25th')
+    df['norm_p75th_income'] = normalize_column(df, 'P75th')
+    df['risk_rating'] = (
+                         (df.norm_ur - df.norm_ur.median()) +
+                         (df.norm_ftr.median() - df.norm_ftr) +
+                         (df.norm_p25th_income.median() - df.norm_p25th_income)
+                         )
+    df['gain_rating'] = (
+                         (df.norm_med_income - df.norm_med_income.median()) +
+                         (df.norm_p75th_income - df.norm_p75th_income.median())
+                         )
     return df
 
 def clean_and_engineer(df):
@@ -49,6 +75,22 @@ def field_majorcat_mapper(series):
     for field, major_cats in outcome_field_dict.iteritems():
         if series['Major_category'] in major_cats:
             return field
+
+def make_hist_box_plot(df, column, box_ratio=0.15, hist_ratio=0.85):
+    sns.set(style="ticks")
+
+    f, (ax_box, ax_hist) = plt.subplots(2,
+                                        sharex=True,
+                                        gridspec_kw={"height_ratios": \
+                                                      (box_ratio, hist_ratio)})
+
+    sns.boxplot(df[column], ax=ax_box)
+    sns.distplot(df[column], ax=ax_hist)
+
+    ax_box.set(yticks=[])
+    sns.despine(ax=ax_hist)
+    sns.despine(ax=ax_box, left=True)
+    plt.show()
 
 def get_outcome_field_dict():
     """
