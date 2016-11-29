8c5da13552d3b7c2199024b8a73a4b48eda45785
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2016-11-29 12:21:30 -0800

    documented and pep8 prettify_output.py

--- src/modeling/outcomes_quiz/prettify_output.py
+++ src/modeling/outcomes_quiz/prettify_output.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-prettified_columns = {'Major_category':'Major Category',
+prettified_columns = {'Major_category': 'Major Category',
                       'P25th': '25th Percentile Income (US$)',
                       'Median': 'Median Income (US$)',
                       'P75th': '75th Percentile Income (US$)',
@@ -18,7 +18,31 @@ final_columns = ['Major',
                  'Median Income (US$)',
                  '75th Percentile Income (US$)']
 
+
 def prettify_final_output(df, pretty_columns, final_columns, num_majors):
+    """
+    Returns a prettified version of the final dataframe for outputting to users
+
+    Parameters
+    ----------
+    df: pandas dataframe
+        Dataframe containing the final results of modeling the user's answers
+
+    pretty_columns: dictionary
+        Columns names to be cleaned up
+        ugly_name(str): pretty_name(str) key-value pairs
+
+    final_columns: list
+        List of columns to included in final dataframe
+
+    num_majors: int
+        The number of majors to include in the outputting
+
+    Returns
+    -------
+    pretty_df: pandas dataframe
+        Dataframe with only the relevant, cleaned column names
+    """
     df['Unemployment Rate (%)'] = df.Unemployment_rate * 100
     df['Full Time Employment Rate (%)'] = df.full_time_employment_rate * 100
     df.rename(columns=pretty_columns, inplace=True)
