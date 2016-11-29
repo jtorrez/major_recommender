14801af6e0d46ddf31ed009db45889d8ef46eb92
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2016-11-29 11:46:17 -0800

    documented and pep8 outcome_cleaning.py

--- src/modeling/outcomes_quiz/outcome_cleaning.py
+++ src/modeling/outcomes_quiz/outcome_cleaning.py
@@ -9,30 +9,84 @@ import operator
 
 def load_data(filename):
     """
+    Returns a pandas dataframe loaded from a csv file.
+
+    Parameters
+    ----------
+    filename: str
+        The filename/path of the csv file to be loaded
+
+    Returns
+    -------
+    df: pandas dataframe
     """
     return pd.read_csv(filename)
 
+
 def drop_features(df):
     """
+    Returns a dataframe where unnecessary rows have been dropped.
+
+    Since this project wasn't concerned with non-4 year degree programs,
+    I dropped all majors that were related to occupational technical degrees
+
+    Would like to refactor this so that what will be dropped isn't hardcoded.
+
+    Parameters
+    ----------
+    df: pandas dataframe
+        Original pandas dataframe of raw data
+
+    Returns
+    -------
+    df_copy: pandas dataframe
+        Copy of original dataframe with appropriate rows dropped
     """
-    df_copy = copy.deepcopy(df[df['Major_category']
-                            != 'Industrial Arts & Consumer Services'])
+    df_copy = copy.deepcopy(df[df['Major_category'] !=
+                            'Industrial Arts & Consumer Services'])
     return df_copy
+
 
 def normalize_column(df, col):
     """
-    Normalize a column by forcing it between 0 and 1
+    Returns a normalized column by forcing all values between 0 and 1
+
+    Parameters
+    ----------
+    df: pandas dataframe
+        The dataframe containing the column to normalized
+    col: str
+        The name of the column to be normalized
+
+    Returns
+    -------
+    norm_col: pandas Series
+        A new column containing the normalized values of the input column
     """
     min_val = df[col].min()
     max_val = df[col].max()
     return (df[col] - min_val) / (max_val - min_val)
 
+
 def add_features(df):
     """
+    Returns same dataframe after adding featured engineered columns inplace
+    to dataframe.
+
+    Would like to refactor this so what is added isn't hardcoded.
+
+    Parameters
+    ----------
+    df: pandas dataframe
+        The original dataframe used in this project
+
+    Returns
+    -------
+    df: pandas dataframe
+        Original dataframe with added columns/features
     """
-    df['full_time_employment_rate'] = (df['Employed_full_time_year_round']
-                                       / df['Employed']
-                                       )
+    df['full_time_employment_rate'] = (df['Employed_full_time_year_round'] /
+                                       df['Employed'])
     df['pay_IQR_range'] = df['P75th'] - df['P25th']
     df['field_of_study'] = df.apply(field_majorcat_mapper, axis=1)
     df['norm_ur'] = normalize_column(df, 'Unemployment_rate')
@@ -51,51 +105,110 @@ def add_features(df):
                          )
     return df
 
+
 def clean_and_engineer(df):
     """
+    Returns a fully cleaned dataframe after dropping and adding features to
+    the input raw dataframe.
+
+    Parameters
+    ----------
+    df: pandas dataframe
+        Raw dataframe for this project
+
+    Returns
+    -------
+    final_df: pandas dataframe
+        Copy of raw dataframe where appropriate features have been dropped and
+        engineereed features have been added.
     """
     clean_df = drop_features(df)
     return add_features(clean_df)
 
+
 def load_and_clean(filename):
     """
+    Loads a csv file and returns a fully cleaned dataframe.
+
+    Parameters
+    ----------
+    filename: str
+        Filename/path of csv file containing raw data for project
+
+    Returns
+    -------
+    cleaned_df: pandas dataframe
+        Fully cleaned dataframe after dropping and adding features to
+        the input file.
     """
     df = load_data(filename)
     return clean_and_engineer(df)
 
+
 def load_clean_write(in_file, out_file):
     """
+    Loads a csv file and writes a new csv file containing fully cleaned data.
+
+    Parameters
+    ----------
+    in_file: str
+        Filename/path of input csv file containing raw data for project.
+
+    Returns
+    -------
+    out_file: str
+        Filename/path of output csv file containing cleaned data for project.
     """
     df = load_and_clean(in_file)
     df.to_csv(out_file, index=False)
     return "Successly wrote file!"
 
+
 def field_majorcat_mapper(series):
     """
+    Returns the appropriate field_of_study based on the major_category of each
+    individual major.
+
+    Parameters
+    ----------
+    series: pandas Series row
+        Row containing a Major_category column
+    Returns
+    -------
+    field: str
+        Field of study label
     """
     outcome_field_dict = get_outcome_field_dict()
     for field, major_cats in outcome_field_dict.iteritems():
         if series['Major_category'] in major_cats:
             return field
 
+
 def get_outcome_field_dict():
     """
+    Returns dictionary mapping field_of_study to major_category
     """
     outcome_field_dict = {
 
-    'Business and Communication': {'Education', 'Business',
-    'Communications & Journalism'},
+     'Business and Communication': {'Education',
+                                    'Business',
+                                    'Communications & Journalism'},
 
-    'Creative Arts': {'Arts'},
+     'Creative Arts': {'Arts'},
 
      'Math, Sciences, and Engineering': {'Engineering',
-     'Biology & Life Science', 'Agriculture & Natural Resources',
-     'Computers & Mathematics', 'Physical Sciences'},
+                                         'Biology & Life Science',
+                                         'Agriculture & Natural Resources',
+                                         'Computers & Mathematics',
+                                         'Physical Sciences'},
 
-     'Public Service, Law, and Policy': {'Health', 'Psychology & Social Work',
-     'Law & Public Policy'},
+     'Public Service, Law, and Policy': {'Health',
+                                         'Psychology & Social Work',
+                                         'Law & Public Policy'},
 
-     'Social Sciences': {'Social Science', 'Humanities & Liberal Arts',
-     'Interdisciplinary'}}
+     'Social Sciences': {'Social Science',
+                         'Humanities & Liberal Arts',
+                         'Interdisciplinary'}
+     }
 
     return outcome_field_dict
